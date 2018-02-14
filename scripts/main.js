bb123c233f0cd55e0e0bce52ccedb7445e5c1b63
gerson97 <gerson.isaias.rivera@gmail.com>
2018-02-14 14:38:28 -0600

    [ADD] Agregue primera parte de CV

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    new WOW().init();
+    // INJECT CSS
+};
